d1efa82afec65625b5986c5069f17ffe559a8284
Darin Webster <darinleenavy@gmail.com>
2019-08-23 15:04:55 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,44 @@
 
+def sort_array_asc(array)
+  array.sort 
+end
+
+def sort_array_desc
+  
+end
+
+def sort_array_char_count(array)
+  array.sort_by { |word| word.length }  
+end
+
+def swap_elements
+  
+end
+
+def reverse_array(array)
+  array.sort do |a, b|
+    if a == b
+      0 
+    elsif a > b
+      -1
+    elsif a < b 
+      1 
+    end
+  end
+end
+
+def kesha_maker
+  
+end
+
+def find_a(array)
+  array.find?("a")
+end
+
+def sum_array(array)
+ array.inject(0) {|sum,x| sum + x }
+end
+
+def add_s(array)
+  array.each {|word| word + "s"}
+end
